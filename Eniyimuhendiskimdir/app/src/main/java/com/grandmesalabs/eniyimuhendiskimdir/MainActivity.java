cb807cb2c83c99e4acd57a6b7f17774e58998e3e
kpmehmetkeskin <mehmet.keskin@koctas.com.tr>
2018-11-13 17:23:17 +0300

    1.2 sürüm cıkıldı

--- Eniyimuhendiskimdir/app/src/main/java/com/grandmesalabs/eniyimuhendiskimdir/MainActivity.java
+++ Eniyimuhendiskimdir/app/src/main/java/com/grandmesalabs/eniyimuhendiskimdir/MainActivity.java
@@ -1,31 +1,19 @@
 package com.grandmesalabs.eniyimuhendiskimdir;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView htmlWebView = (WebView)findViewById(R.id.web_view);
-        htmlWebView.setWebViewClient(new CustomWebViewClient());
-        WebSettings webSetting = htmlWebView.getSettings();
-        webSetting.setJavaScriptEnabled(true);
-        webSetting.setDisplayZoomControls(true);
-        htmlWebView.loadUrl("http://www.eniyimuhendiskimdir.com/");
-    }
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.eniyimuhendiskimdir.com/"));
+        startActivity(browserIntent);
 
-    private class CustomWebViewClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
     }
 }
