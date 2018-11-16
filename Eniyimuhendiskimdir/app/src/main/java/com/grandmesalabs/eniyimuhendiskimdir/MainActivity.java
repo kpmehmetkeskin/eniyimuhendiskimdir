33c15e3c3b9edd22951678cfde270bf836d40d9e
kpmehmetkeskin <mehmet.keskin@koctas.com.tr>
2018-11-16 16:00:24 +0300

    1.3 sürümü: Site, webview içine alındı.

--- Eniyimuhendiskimdir/app/src/main/java/com/grandmesalabs/eniyimuhendiskimdir/MainActivity.java
+++ Eniyimuhendiskimdir/app/src/main/java/com/grandmesalabs/eniyimuhendiskimdir/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
@@ -12,8 +13,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.eniyimuhendiskimdir.com/"));
-        startActivity(browserIntent);
+        //Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.eniyimuhendiskimdir.com/"));
+        //startActivity(browserIntent);
+
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.eniyimuhendiskimdir.com/");
 
     }
 }
